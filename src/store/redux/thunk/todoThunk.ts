bfaf2d081b458e070a12a17e605c706847238270
Max <markushitstep@gmail.com>
2024-02-12 13:20:30 +0200

    add types to thunk getTodos

--- src/store/redux/thunk/todoThunk.ts
+++ src/store/redux/thunk/todoThunk.ts
@@ -1,47 +1,14 @@
 import { API } from 'src/api';
-import { TodoActions, TodoThunk } from '../types/todo';
-import axios from 'axios';
-import { Dispatch, UnknownAction } from 'redux';
-import { ThunkDispatch, createAsyncThunk } from '@reduxjs/toolkit';
-import { RootState } from '../reducers';
+import { TodoThunk } from '../types/todo';
+import { Dispatch } from 'redux';
+import { getTodosActionCreator } from '../actionCreator/todo';
 
-export const getTodos = () => {
-  return async () => {
+export const getTodos = (): TodoThunk => {
+  return async (dispatch: Dispatch) => {
     try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos',
-      );
+      const response = await API.get('/todos');
 
-      console.log(response);
+      dispatch(getTodosActionCreator(response.data));
     } catch (error) {}
   };
 };
-
-export const fetchTodos = () => {
-  return async () => {
-    try {
-      //dispatch(fetchTodosRequest());
-
-      // Здесь вы можете выполнить асинхронный запрос, например, к API
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos',
-      );
-      //const data: Todo[] = await response.json();
-      console.log(response);
-      //dispatch(fetchTodosSuccess(data));
-    } catch (error) {
-      //dispatch(fetchTodosFailure(error.message));
-    }
-  };
-};
-
-// export const getTodos = createAsyncThunk(
-//   'https://jsonplaceholder.typicode.com/todos',
-//   async () => {
-//     const response = await axios.get(
-//       'https://jsonplaceholder.typicode.com/todos',
-//     );
-//     console.log('dsadsa');
-//     return response.data;
-//   },
-// );
